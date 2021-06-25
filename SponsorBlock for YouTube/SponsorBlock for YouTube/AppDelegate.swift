3f46196657b63bd17780bf9213f9aec7931e8fba
Ajay Ramachandran <dev@ajay.app>
2021-06-24 23:29:14 -0400

    Initial Commit

---
+++ SponsorBlock for YouTube/SponsorBlock for YouTube/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SponsorBlock for YouTube
+//
+//  Created by user927005 on 6/22/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
